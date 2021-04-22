deb2d5a573084e413f4e8e807a02f94400da43b7
gurubasavraj15 <77735924+gurubasavraj15@users.noreply.github.com>
2021-04-22 16:06:46 +0530

    Create subtraction.py

---
+++ subtraction.py
@@ -0,0 +1,6 @@
+num1 = int(input("Please enter the first number!\n"));
+num2 = int(input("Please enter the second number!\n"));
+
+difference = num1-num2;
+
+print("Difference between "+str(num1)+" and "+str(num2)+" is "+str(difference));
