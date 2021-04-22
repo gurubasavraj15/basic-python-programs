e4be1ddd80c0b270c87eedaee8194c23cd2f270c
gurubasavraj15 <77735924+gurubasavraj15@users.noreply.github.com>
2021-04-22 16:10:20 +0530

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,6 @@
+num1 = int(input("Please enter the first number!\n"));
+num2 = int(input("Please enter the second number!\n"));
+
+product = num1*num2;
+
+print("Product of "+str(num1)+" and "+str(num2)+" is "+str(product));
