2147d6ad466acc5cbe4f7698c594629569125e15
gurubasavraj15 <77735924+gurubasavraj15@users.noreply.github.com>
2021-04-22 16:05:30 +0530

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,6 @@
+num1 = int(input("Please enter the first number!\n"));
+num2 = int(input("Please enter the second number!\n"));
+
+sum = num1+num2;
+
+print("Sum of "+str(num1)+" and "+str(num2)+" is "+str(sum));
